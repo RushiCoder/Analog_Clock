48761c12b7e20c750acb566309aabf452cb6f720
Rushikesh Chincholkar <114005115+RushiCoder@users.noreply.github.com>
2023-02-21 01:35:56 +0530

    Add files via upload
    This Clock is Made for practice.

---
+++ project2.js
@@ -0,0 +1,29 @@
+function getAngle() {
+    // get hand elements
+    const hourHand = document.querySelector('.hour');
+    const minuteHand = document.querySelector('.minute');
+    const secondHand = document.querySelector('.second');
+
+    //hours, minutes and seconds
+    const today = new Date(); //Date object
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    //angle of rotation of hands
+    let hrHandRotation = 30*hours + minutes/2 + seconds/120;
+    let minHandRotation = 6*minutes + seconds/10;
+    let secHandRotation = 6*seconds;
+
+    //now rotate the hands
+    //adding traslateX here otherwise only rotates works
+    hourHand.style.transform = `traslateX(-50%) rotate(${hrHandRotation}deg)`;
+    minuteHand.style.transform = `translateX(-50%) rotate(${minHandRotation}deg)`;
+    secondHand.style.transform = `translateX(-50%) rotate(${secHandRotation}deg)`;
+
+}
+
+getAngle();
+//to start the clock as soon as page loads otherwise it jumps to current time abruptly form 12
+
+setInterval(getAngle, 1000);
